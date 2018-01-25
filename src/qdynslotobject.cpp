206c4121a8e6e52a56fbaf4d47a514b0d88d086b
drswinghead <drswinghead@163.com>
2018-01-25 22:09:18 +0800

    add universal signal/slot set and callback feature.

---
+++ src/qdynslotobject.cpp
@@ -0,0 +1,166 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'qdynslotobject.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.10.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "qdynslotobject.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'qdynslotobject.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.10.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#include <cassert>
+#include <QtCore>
+
+void QDynSlotObject::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    Q_UNUSED(_o);
+    Q_UNUSED(_id);
+    Q_UNUSED(_c);
+    Q_UNUSED(_a);
+
+    QDynSlotObject* this_ = (QDynSlotObject*)_o;
+    // qDebug()<<"calling:"<< _o << _c << _id << this_->fnptr_;
+    if (this_->fnptr_ != 0) {
+        this_->fnptr_(_o, _c, _id, _a, this_->name_,
+                      this_->argc_, this_->argtys_, this_->cbptr_);
+    }
+    // qDebug()<<"called:"<< _o << _c << _id;
+}
+
+const QMetaObject *QDynSlotObject::metaObject() const
+{
+    // qDebug()<<"hehehhe"<< QObject::d_ptr->metaObject;
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *QDynSlotObject::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, this->qt_meta_stringdata.stringdata0))
+        return static_cast<void*>(this);
+    return QObject::qt_metacast(_clname);
+}
+
+int QDynSlotObject::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    // qDebug()<<"called:"<<_c << _id;
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 1)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 1;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 1)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 1;
+    }
+    return _id;
+}
+
+
+/////////////////////
+static QAtomicInt dyn_slot_obj_seq = 0;
+QDynSlotObject::QDynSlotObject(void *fnptr, char* name, int argc, int*argtys, void*cbptr) : QObject()
+{
+    this->setCallbackSlot(fnptr, name, argc, argtys, cbptr);
+}
+
+QDynSlotObject::~QDynSlotObject() {}
+
+void QDynSlotObject::setCallbackSlot(void *fnptr, char* name, int argc, int*argtys, void*cbptr)
+{
+    fnptr_ = (void(*)(QObject*, int, int, void*, char*, int, int*, void*))fnptr;
+    name_ = name;
+    argc_ = argc;
+    argtys_ = argtys;
+    cbptr_ = cbptr;
+
+    // rewrite qt_meta_stringdata_ and qt_meta_data_ and staticMetaObject
+    dyn_slot_obj_seq += 1;
+    uint64_t rdseq = qrand(); rdseq |= uint64_t(qrand()) << 32;
+    QByteArray rdseqbin((char*)&rdseq, 8);
+    char* rdseqhex = rdseqbin.toHex().toUpper().data();
+    // qDebug()<<rdseqbin.toHex().toUpper();
+    assert(strlen(rdseqhex) == 16);
+    strcpy(&this->qt_meta_stringdata.stringdata0[14], rdseqhex);
+
+    this->staticMetaObject = {
+         { &QObject::staticMetaObject, this->qt_meta_stringdata.data,
+           this->qt_meta_data, qt_static_metacall, nullptr, nullptr},
+    };
+}
+
+extern "C" void callback_slot_test(void*_o, int _c, int _id, void*_a,
+                                   char*name, int argc, int*argtys, void*cbptr) {
+    qDebug()<<"callback_slot called:"<<_o<<_c<<_id<<_a << ((QDynSlotObject*)_o)->name_;
+    void**ap = (void**)_a;
+    const QString* newname = (const QString*)(ap[1]);
+    qDebug()<<"new object name from signal parameter 0:" << newname->toLatin1().data() << ap[1];
+}
+
+// see mkuse/cffiqt/tsym/
+int main_test(int argc, char **argv)
+{
+    QCoreApplication app(argc, argv);
+    qsrand(time(NULL));
+
+    // qDebug() << "Basic QObject class size: " << sizeof(QDemoObject); // 16
+    qDebug() << "Deep Custom QObject class size: " << sizeof(QDynSlotObject);
+    qDebug() << "QMetaObject class size: " << sizeof(QMetaObject);
+    qDebug() << "meta stringdata class size: " << sizeof(struct QDynSlotObject::qt_meta_stringdata_QDynSlotObject_t);
+
+    QDynSlotObject* dso = new QDynSlotObject((void*)&callback_slot_test, "clicked123", 0, 0, 0);
+    QMetaObject* mto = (QMetaObject*)dso->metaObject();
+    qDebug() << (void*)(mto->className())  << mto->methodCount();
+    qDebug() << mto->className()  << mto->methodCount();
+    for (int i = 0; i < mto->methodCount(); i ++) {
+        qDebug()<<mto->method(i).name()<<mto->method(i).methodSignature();
+    }
+
+    QDynSlotObject* dso2 = new QDynSlotObject((void*)&callback_slot_test, "released456", 0, 0, 0);
+    QObject::connect(dso, SIGNAL(objectNameChanged(const QString &)), dso2, SLOT(dumnyslot()));
+    dso->setObjectName(QString("beginnotdso111"));
+    dso->setObjectName(QString("beginnotdso222"));
+
+    // app.exec();
+    return 0;
+}
+
+/////
+QDynSlotObject* QDynSlotObject_new(void *fnptr, char* name, int argc, int*argtys, void* cbptr) {
+    QCoreApplication::instance();
+    return new QDynSlotObject(fnptr, name, argc, argtys, cbptr);
+}
+
+void QDynSlotObject_delete(QDynSlotObject*o) { if (o != NULL) { delete o; } }
+
+void QDynSlotObject_connect_switch(QObject*src, char*signature, QDynSlotObject*me, int on) {
+    qDebug()<<src<<signature<<me<<on;
+    qDebug()<<((QTimer*)src)->interval();
+    qDebug()<<((QTimer*)src)->isActive();
+    qDebug()<<((QTimer*)src)->remainingTime();
+    if (on == 0) {
+        QObject::disconnect(src, signature, me, SLOT(dumnyslot()));
+    } else {
+        QObject::connect(src, signature, me, SLOT(dumnyslot()));
+    }
+}
+
+void QDynSlotObject_connect_test(QObject*ofrom, QObject*oto) {
+    QObject::connect(ofrom, SIGNAL(objectNameChanged(const QString &)), oto, SLOT(dumnyslot()));
+}
+
+void QDynSlotObject_trigger_test(QObject*osrc) {
+    osrc->setObjectName(QString("rdobjname:")+QString::number(qrand()));
+}
---
+++ src/qdynslotobject.h
@@ -0,0 +1,96 @@
+#ifndef _QDynSlotObject_H_
+#define _QDynSlotObject_H_
+
+#include <QObject>
+
+
+class QDynSlotObject : public QObject
+{
+    // Q_OBJECT
+ public:
+    QMetaObject staticMetaObject;
+    virtual const QMetaObject *metaObject() const;
+    virtual void *qt_metacast(const char *);
+    virtual int qt_metacall(QMetaObject::Call, int, void **);
+    static inline QString tr(const char *s, const char *c = nullptr, int n = -1)
+    { return QObject::staticMetaObject.tr(s, c, n); }
+    static inline QString trUtf8(const char *s, const char *c = nullptr, int n = -1)
+    { return QObject::staticMetaObject.tr(s, c, n); }
+ private:
+    __attribute__((visibility("hidden"))) static void qt_static_metacall(QObject *, QMetaObject::Call, int, void **);
+    struct QPrivateSignal {};
+
+ public:
+    QDynSlotObject(void *fnptr, char* name, int argc, int*argtys, void* cbptr);
+    virtual ~QDynSlotObject();
+    void setCallbackSlot(void *fnptr, char* name, int argc, int*argtys, void* cbptr);
+
+    void (*fnptr_)(QObject*, int, int, void*, char*name, int argc, int*argtys, void*cbptr) = 0;
+    char* name_;
+    int argc_;
+    int* argtys_;
+    void*cbptr_;
+
+ public:
+    ///
+    struct qt_meta_stringdata_QDynSlotObject_t {
+        QByteArrayData data[3];
+        char stringdata0[128];  // classname123\0firstslotname\0\0
+    };
+
+#define _QT_MOC_LITERAL_TMP(idx, ofs, len)                              \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len,        \
+    qptrdiff(offsetof(qt_meta_stringdata_QDynSlotObject_t, stringdata0) + ofs \
+             - idx * sizeof(QByteArrayData))                            \
+     )
+
+    struct qt_meta_stringdata_QDynSlotObject_t qt_meta_stringdata = {
+        {
+            _QT_MOC_LITERAL_TMP(0, 0, 30), // "QDemoObject"
+            _QT_MOC_LITERAL_TMP(1, 31, 9), // "dummyslot"
+            _QT_MOC_LITERAL_TMP(2, 41, 0) // ""
+        },
+        "QDynSlotObject0123456789ABCDEF\0dumnyslot\0"
+        // uint64的object编号
+        };
+#undef _QT_MOC_LITERAL_TMP
+
+    uint qt_meta_data[128] = {
+        // content:
+        7,       // revision
+        0,       // classname
+        0,    0, // classinfo
+        1,   14, // methods
+        0,    0, // properties
+        0,    0, // enums/sets
+        0,    0, // constructors
+        0,       // flags
+        0,       // signalCount
+
+        // slots: name, argc, parameters, tag, flags
+        1,    0,   19,    2, 0x0a /* Public */,
+
+        // slots: parameters
+        QMetaType::Void,
+
+        0        // eod
+    };
+};
+
+// TODO 放在qt.inline项目中吧，
+// 注意，尽量避免链接进进程，尽量使用ffi_call
+#ifdef __cplusplus
+extern "C"{
+#endif
+    QDynSlotObject* QDynSlotObject_new(void *fnptr, char* name, int argc, int*argtys, void* cbptr);
+    void QDynSlotObject_delete(QDynSlotObject*o);
+    void QDynSlotObject_connect_switch(QObject*src, char*signature, QDynSlotObject*me, int on);
+    void QDynSlotObject_connect_test(QObject*ofrom, QObject*oto);
+    void QDynSlotObject_trigger_test(QObject*osrc);
+#ifdef __cplusplus
+};
+#endif
+
+#endif
+
+
