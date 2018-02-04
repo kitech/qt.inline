13822ddf55fa4ab549746b0aa6604b08ab42ef7e
drswinghead <drswinghead@163.com>
2018-02-04 23:35:15 +0800

    basic android support.

--- src/qihotfix.cpp
+++ src/qihotfix.cpp
@@ -12,6 +12,7 @@ void _ZN7QScreen10grabWindowEiiiii(void* qthis, int a0, int a1, int a2, int a3, 
     that->grabWindow((WId)a0, a1, a2, a3, a4);
 }
 
+/*
 #include <qopenglversionfunctions.h>
 extern "C"
 void* _ZN24QAbstractOpenGLFunctions6d_funcEv(void* qthis)
@@ -19,6 +20,7 @@ void* _ZN24QAbstractOpenGLFunctions6d_funcEv(void* qthis)
     QAbstractOpenGLFunctions* that = (QAbstractOpenGLFunctions*)qthis;
     return that->d_func();
 }
+*/
 
 extern "C"
 void _ZN18QMessageLogContext4copyERKS_(void* qthis, const QMessageLogContext &logContext)
