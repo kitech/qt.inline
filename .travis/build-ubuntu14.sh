b257c40fc2809467c8a518090bf0def5bee3ba10
drswinghead <drswinghead@gmail.com>
2018-12-31 12:08:29 +0800

    fix ubuntu14 static build of clang version

--- .travis/build-ubuntu14.sh
+++ .travis/build-ubuntu14.sh
@@ -10,8 +10,20 @@ cat /etc/issue
 cat /etc/os-release
 
 # not docker, need sudo
+# clang 6.0
+sudo sh -c "echo 'deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-6.0 main' >> /etc/apt/sources.list"
+sudo sh -c "echo 'deb-src http://apt.llvm.org/trusty/ llvm-toolchain-trusty-6.0 main' >> /etc/apt/sources.list"
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
+tail /etc/apt/sources.list
+sudo apt-get -qq update
+apt-cache search clang
+
 sudo apt-get -qq update
 sudo apt-get install -y libx11-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-keysyms1-dev libxcb-present-dev libxcb-present-dev libxcb-shm0-dev libxcb-sync-dev libxcb-xfixes0-dev libxcb-xkb-dev libxcb-cursor-dev libxcb-xinerama0-dev libice-dev
+sudo apt-get install -y software-properties-common python-software-properties
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get -qq update
+sudo apt-get install -y clang-6.0
 
 source ./.qtenvs
 env
@@ -20,10 +32,18 @@ ls
 
 # script:
 pwd
-export CC=clang CXX=clang++
+if [[ $QTVER = "v5.9" ]] || [[ $QTVER = "v5.10" ]]; then
+    export CC=clang CXX=clang++
+else
+    cd /usr/bin/ && sudo ln -sv clang++-6.0 clang++ && cd -
+    cd /usr/bin/ && sudo ln -sv clang-6.0 clang && cd -
+    export CC=clang-6.0 CXX=clang++-6.0
+fi
+
 if [ x"$WITH_QT_STATIC" == x"" ]; then
     WITH_QT_STATIC=off
 fi
+
 qtverdir=$(echo "$QTVER"|sed 's/v//'|sed 's/\.//')  # v5.10 => 510
 if [ x"$WITH_QT_STATIC" == x"on" ]; then
     if [[ $QTVER = "v5.9" ]] || [[ $QTVER = "v5.10" ]]; then
@@ -36,16 +56,8 @@ if [ x"$WITH_QT_STATIC" == x"on" ]; then
         export PATH=/opt/qt512st/bin:$PATH
     fi
 else
-    if [[ $QTVER = "v5.9" ]]; then
-        git clone https://github.com/qtchina/qt59_linux_gcc64.git
-        export PATH=`pwd`/qt59_linux_gcc64/bin:$PATH
-    elif [[ $QTVER = "v5.10" ]]; then
-        git clone https://github.com/qtchina/qt510_linux_gcc64.git
-        export PATH=`pwd`/qt510_linux_gcc64/bin:$PATH
-    else # master
-        git clone https://github.com/qtchina/qt512_linux_gcc64.git
-        export PATH=`pwd`/qt512_linux_gcc64/bin:$PATH
-    fi
+    git clone https://github.com/qtchina/qt${qtverdir}_linux_gcc64.git
+    export PATH=`pwd`/qt${qtverdir}_linux_gcc64/bin:$PATH
 fi
 cmake -DWITH_QT_STATIC=$WITH_QT_STATIC .
 
